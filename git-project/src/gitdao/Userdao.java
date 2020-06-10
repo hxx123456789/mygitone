6efc1a42c9fd3bc038a99d4ca18992794a48222d
hxx123456789 <630187154@qq.com>
2020-06-10 11:21:50 +0800

    提交到本地仓库

---
+++ git-project/src/gitdao/Userdao.java
@@ -0,0 +1,23 @@
+package gitdao;
+
+import java.util.List;
+
+public class Userdao {
+	public void adduser(int id,String username,String password) {
+		
+	}
+
+	public void deleteUser(int id) {
+		
+	}
+	
+	public void updateUser(int id, String newUsername,String newPassSize) {
+		
+	}
+	
+	public List<Object> getUsers (int pageIndex,int pageSize){
+	
+		return null;
+	}
+	
+}
